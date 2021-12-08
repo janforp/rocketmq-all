c26bec0e09b3d13c3972505ff2794ec8217dfb49
zhucj <zhucj@servyou.com.cn>
2021-12-08 11:18:51 +0800

    namesrv

--- common/src/main/java/org/apache/rocketmq/common/protocol/MQProtosHelper.java
+++ common/src/main/java/org/apache/rocketmq/common/protocol/MQProtosHelper.java
@@ -11,8 +11,7 @@ public class MQProtosHelper {
 
     private static final InternalLogger log = InternalLoggerFactory.getLogger(LoggerName.COMMON_LOGGER_NAME);
 
-    public static boolean registerBrokerToNameServer(final String nsaddr, final String brokerAddr,
-            final long timeoutMillis) {
+    public static boolean registerBrokerToNameServer(final String nsaddr, final String brokerAddr, final long timeoutMillis) {
         RegisterBrokerRequestHeader requestHeader = new RegisterBrokerRequestHeader();
         requestHeader.setBrokerAddr(brokerAddr);
 
