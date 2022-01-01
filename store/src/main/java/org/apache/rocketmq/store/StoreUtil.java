c10076310213b69f3b3533cefee218e52a321ed3
zhucj <zhucj@servyou.com.cn>
2022-01-01 09:48:00 +0800

    org.apache.rocketmq.store.DefaultMessageStore.getMessage

--- store/src/main/java/org/apache/rocketmq/store/StoreUtil.java
+++ store/src/main/java/org/apache/rocketmq/store/StoreUtil.java
@@ -14,7 +14,28 @@ public class StoreUtil {
         if (osmxb instanceof com.sun.management.OperatingSystemMXBean) {
             physicalTotal = ((com.sun.management.OperatingSystemMXBean) osmxb).getTotalPhysicalMemorySize();
         }
-
         return physicalTotal;
+    }
+
+    public static void main(String[] args) {
+        OperatingSystemMXBean osmxb = ManagementFactory.getOperatingSystemMXBean();
+        System.out.println(osmxb.getObjectName());
+        System.out.println(osmxb.getName());
+        System.out.println(osmxb.getAvailableProcessors());
+        System.out.println(osmxb.getArch());
+        System.out.println(osmxb.getVersion());
+        System.out.println(osmxb.getSystemLoadAverage());
+        if (osmxb instanceof com.sun.management.OperatingSystemMXBean) {
+            com.sun.management.OperatingSystemMXBean bean = (com.sun.management.OperatingSystemMXBean) osmxb;
+            System.out.println(bean.getCommittedVirtualMemorySize());
+            System.out.println(bean.getTotalSwapSpaceSize());
+            System.out.println(bean.getFreeSwapSpaceSize());
+            System.out.println(bean.getProcessCpuTime());
+            System.out.println(bean.getFreePhysicalMemorySize());
+            System.out.println(bean.getTotalPhysicalMemorySize());
+            System.out.println(bean.getSystemCpuLoad());
+            System.out.println(bean.getProcessCpuLoad());
+        }
+
     }
 }
