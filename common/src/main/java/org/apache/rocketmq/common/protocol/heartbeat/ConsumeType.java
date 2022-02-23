175737e7c08673a1998211228e8cbf1e03df8c45
zhucj <zhucj@servyou.com.cn>
2022-02-23 14:08:16 +0800

    truncateMessageQueueNotMyTopic

--- common/src/main/java/org/apache/rocketmq/common/protocol/heartbeat/ConsumeType.java
+++ common/src/main/java/org/apache/rocketmq/common/protocol/heartbeat/ConsumeType.java
@@ -7,8 +7,17 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ConsumeType {
 
+    /**
+     * 积极消费
+     * 主动去拉
+     */
     CONSUME_ACTIVELY("PULL"),
 
+    /**
+     * 被动消费
+     *
+     * 被动推过来
+     */
     CONSUME_PASSIVELY("PUSH");
 
     private final String typeCN;
